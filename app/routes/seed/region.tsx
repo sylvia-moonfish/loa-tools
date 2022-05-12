db0acd0d47b719f40ea5a08796dc8d6c9bb1bc84
sylvia-moonfish <46030655+sylvia-moonfish@users.noreply.github.com>
2022-05-11 17:05:36 -0700

    added route for seeding region

---
+++ app/routes/seed/region.tsx
@@ -0,0 +1,136 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { prisma } from "~/db.server";
+
+export const loader: LoaderFunction = async () => {
+  const regionMap = [
+    {
+      regionName: "North America West",
+      serverNames: [
+        "Mari",
+        "Valtan",
+        "Enviska",
+        "Akkan",
+        "Bergstrom",
+        "Shandi",
+        "Rohendel",
+      ],
+    },
+    {
+      regionName: "North America East",
+      serverNames: [
+        "Azena",
+        "Una",
+        "Regulus",
+        "Avesta",
+        "Galatur",
+        "Karta",
+        "Ladon",
+        "Kharmine",
+        "Elzowin",
+        "Sasha",
+        "Adrinne",
+        "Aldebaran",
+        "Zosma",
+        "Vykas",
+        "Danube",
+      ],
+    },
+    {
+      regionName: "Europe Central",
+      serverNames: [
+        "Neria",
+        "Kadan",
+        "Trixion",
+        "Calvasus",
+        "Thirain",
+        "Zinnervale",
+        "Asta",
+        "Wei",
+        "Slen",
+        "Sceptrum",
+        "Procyon",
+        "Beatrice",
+        "Inanna",
+        "Thaemine",
+        "Sirius",
+        "Antares",
+        "Brelshaza",
+        "Nineveh",
+        "Mokoko",
+      ],
+    },
+    {
+      regionName: "Europe West",
+      serverNames: [
+        "Rethramis",
+        "Tortoyk",
+        "Moonkeep",
+        "Stonehearth",
+        "Shadespire",
+        "Tragon",
+        "Petrania",
+        "Punika",
+      ],
+    },
+    {
+      regionName: "South America",
+      serverNames: [
+        "Kazeros",
+        "Agaton",
+        "Gienah",
+        "Arcturus",
+        "Yorn",
+        "Feiton",
+        "Vern",
+        "Kurzan",
+        "Prideholme",
+      ],
+    },
+  ];
+
+  for (let i = 0; i < regionMap.length; i++) {
+    let region = await prisma.region.upsert({
+      where: {
+        name: regionMap[i].regionName,
+      },
+      update: {
+        name: regionMap[i].regionName,
+      },
+      create: {
+        name: regionMap[i].regionName,
+      },
+    });
+
+    for (let j = 0; j < regionMap[i].serverNames.length; j++) {
+      await prisma.server.upsert({
+        where: {
+          name: regionMap[i].serverNames[j],
+        },
+        update: {
+          name: regionMap[i].serverNames[j],
+          regionId: region.id,
+        },
+        create: {
+          name: regionMap[i].serverNames[j],
+          regionId: region.id,
+        },
+      });
+    }
+  }
+
+  return json(
+    await prisma.region.findMany({
+      orderBy: {
+        name: "asc",
+      },
+      include: {
+        servers: {
+          orderBy: {
+            name: "asc",
+          },
+        },
+      },
+    })
+  );
+};
